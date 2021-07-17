7bd9a8fe1358553835697bcc1a0d1143583697c1
sarveshhome <sarvesh.carlton@gmail.com>
2021-07-17 13:07:15 +0100

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { BookingComponent } from './booking/booking.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, BookingComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
