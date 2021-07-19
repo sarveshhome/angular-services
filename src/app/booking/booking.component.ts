a7b0ef1d5dca363410afe5ced8f83752474cff4f
sarveshhome <sarvesh.carlton@gmail.com>
2021-07-19 08:02:07 +0100

    1 file modified

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { appstateService } from '../shared/appstate.service';
 
 @Component({
@@ -8,8 +9,19 @@ import { appstateService } from '../shared/appstate.service';
 })
 export class BookingComponent implements OnInit {
   message: string;
+  id: number;
+  private sub: any;
+  constructor(
+    private appstate: appstateService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
-  constructor(private appstate: appstateService) {}
-
-  ngOnInit() {}
+  ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.id = +params.id; // (+) converts string 'id' to a number
+      console.log(this.id);
+      // In a real app: dispatch action to load the details here.
+    });
+  }
 }
