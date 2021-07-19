5a3ae581b3c6bed9b600150d3c4f254992f52fd5
sarveshhome <sarvesh.carlton@gmail.com>
2021-07-19 07:36:57 +0100

    4 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,9 @@ import { AboutComponent } from './about/about.component';
 import { BookingComponent } from './booking/booking.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { HotelaComponent } from './hotela/hotela.component';
+import { HotelbComponent } from './hotelb/hotelb.component';
+import { HotelcComponent } from './hotelc/hotelc.component';
 import { NoPageFoundComponent } from './no-page-found/no-page-found.component';
 
 const routes: Routes = [
@@ -11,6 +14,9 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'hotela', component: HotelaComponent },
+  { path: 'hotelb', component: HotelbComponent },
+  { path: 'hotelc', component: HotelcComponent },
   { path: 'booknow', component: BookingComponent },
   { path: '**', component: NoPageFoundComponent }
 ];
